257ecba2988c4c6273da7e6eac0b8589aad686c7
KaroAntonio <karoantonio@gmail.com>
2017-11-20 15:15:29 -0500

    non stroke

--- script.js
+++ script.js
@@ -5,29 +5,28 @@ var ctr = 0;
 var width = window.innerWidth;
 var height = window.innerHeight;
 
+var band_colors = []
+var bands = []
 
 $(document).ready(function() {
 	$('#loading').hide();
 	
 	center($('#image'))		
 
-
 	$('#words').css({
 		fontSize:height/8,
-		color:'white',
+		color:'black',
 		width:width,
 		height:height,
 		position:'fixed',
 		textAlign:'center',	
-		paddingTop:height/2-(height/16*2),
 	});
 	
 	var edge = height;
-	$('#color').css({
-		height: height,
-		width:width,
-	});
+
 	
+	init_color_bands();
+
 	cycle();
 
 	$(document).mousemove(function() {
@@ -36,6 +35,47 @@ $(document).ready(function() {
 
 })
 
+
+function init_color_bands() {
+
+	
+	var band_height = height/256;
+	n_bands = Math.round(height*0.7/band_height);
+	console.log(n_bands)
+
+	for (var i =0; i<n_bands; i++) {
+		var band = $('<div>');
+		var color = getRandomColor();
+		band_colors.push(color);
+		bands.push(band);
+		band.css({
+			background:'black',
+			margin_top: band_height*i,
+			height: band_height,
+			width:width,
+			borderColor: 'black',
+			opacity: 0.2,
+		});
+
+		$('body').prepend(band);
+	}	
+	color_bands();
+
+}
+
+function add_new_color() {
+	band_colors.push(getRandomColor())
+	band_colors.shift()
+}
+
+function color_bands() {
+	
+	for (var i =0; i<bands.length; i++) {
+		bands[i].css({
+			background:band_colors[i],
+		});	
+	}
+}
 
 function center(el) {
 	el.load(function () {
@@ -49,15 +89,10 @@ function center(el) {
 
 function cycle() {
 	ctr += 1;
-	if (ctr%2) {
-		$('#image').show();
-		$('#words').hide();
-	} else {
-		$('#words').show();
-		//$('#words').css({color:getRandomColor()});
-		$('#image').hide();
-	}
 	set_background_color();
+	add_new_color();
+	color_bands();
+
 }
 
 function set_background_color() {
@@ -70,7 +105,7 @@ function getRandomColor() {
 	var letters = '0123456789ABCDEF';
 	var color = '#';
 	for (var i = 0; i < 6; i++) {
-	color += letters[Math.floor(Math.random() * 16)];
-		}
+		color += letters[Math.floor(Math.random() * 16)];
+	}
 	return color;
 }
